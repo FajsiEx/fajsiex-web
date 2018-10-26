61b7fa89b3d79ac3c5abbfa16a005ae2f6e9c7f8
FajsiEx <fajsiexofficial@gmail.com>
2018-10-26 19:37:05 +0200

    Added a preloader to /home/ page

--- includes/load.js
+++ includes/load.js
@@ -1,4 +1,10 @@
 $(()=>{
     $("#head-content").load("../includes/header.html"); 
-    $("#foot-content").load("../includes/footer.html"); 
+    $("#foot-content").load("../includes/footer.html");
+    onload = ()=> { 
+        $("#preloader").css("opacity", "0");
+        setTimeout(()=>{
+            $("#preloader").css("display", "none");
+        }, 275);
+    }
 });
