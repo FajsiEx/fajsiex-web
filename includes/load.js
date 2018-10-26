9c6a098e257638e1c294608e6c7e159b3e6ef2cd
FajsiEx <fajsiexofficial@gmail.com>
2018-10-26 21:48:21 +0200

    Added the new includes to reqm and fixed the includes loader

--- includes/load.js
+++ includes/load.js
@@ -1,10 +1,10 @@
 $(()=>{
-    $("#head-content").load("../includes/header.html"); 
-    $("#foot-content").load("../includes/footer.html");
-    onload = ()=> { 
-        $("#preloader").css("opacity", "0");
-        setTimeout(()=>{
-            $("#preloader").css("display", "none");
-        }, 275);
-    }
+    $("#head-content").load("../includes/header.html", ()=> {
+        $("#foot-content").load("../includes/footer.html", ()=>{
+            $("#preloader").css("opacity", "0");
+            setTimeout(()=>{
+                $("#preloader").css("display", "none");
+            }, 275);
+        });
+    }); 
 });
