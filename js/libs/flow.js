fab0d0518f7d9b8feb1c2cb6b6a584e1abbb86c6
FajsiEx <fajsiexofficial@gmail.com>
2018-12-28 16:57:04 +0100

    update / updated the flow lib

--- js/libs/flow.js
+++ js/libs/flow.js
@@ -3,8 +3,17 @@ $('.flow-block').tilt({
     perspective: 1000,
 })
 $('.flow-button').tilt({
-    maxTilt: 7.5,
+    maxTilt: 5,
     perspective: 1500,
     glare: true,
     maxGlare: .25
-})
+});
+
+$(".flow-toggle input").on("click", (e)=>{
+    console.log(e);
+    if (e.target.checked) {
+        $(e.target.parentElement).addClass("flow-toggle-on");
+    }else{
+        $(e.target.parentElement).removeClass("flow-toggle-on");
+    }
+});
