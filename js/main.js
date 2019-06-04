f525a3670ef6f40f248bb9f20347165a8997222c
FajsiEx <fajsiexofficial@gmail.com>
2019-06-04 22:48:51 +0200

    + paralax for the background

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ onload = () => {
         console.log('callback - particles.js config loaded');
     });
 };
+
+$(document).mousemove((event)=> {
+    let xMult = event.clientX / innerWidth;
+    let yMult = event.clientY / innerHeight;
+
+    let left = - (xMult*10) / 4;
+    let top =  - (yMult*10) / 4;
+
+    $(".background").css("left", `${left}%`);
+    $(".background").css("top", `${top}%`);
+});
