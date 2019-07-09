15b82d0010819cad9402f7aa174cc6688d7c6aa3
FajsiEx <fajsiexofficial@gmail.com>
2019-07-09 11:14:12 +0200

    + fetch timelineData

--- js/main.js
+++ js/main.js
@@ -35,15 +35,6 @@ function generateChangelogChangesHtml(changes) {
     return outputHtml;
 }
 
-function generateReleaseTagsHtml(tags) {
-    if (!tags) { return ""; }
-    let outputHtml = "";
-    for (let tag of tags) {
-        outputHtml += /*html*/`<div class="release-tag tag-${tag}">${tag.toUpperCase()}</div>`;
-    }
-    return outputHtml;
-}
-
 // $(document).mousemove((event)=> {
 //     let xMult = event.clientX / innerWidth;
 //     let yMult = event.clientY / innerHeight;
@@ -69,111 +60,51 @@ function generateReleaseTagsHtml(tags) {
 },
 */
 
-const timelineData = [
-    {
-        type: "release",
-        project: "Website",
-        build: "19.7.9",
-        changes: {
-            added: [
-                "Dark gradient background to navbar",
-                "Finally added the footer",
-                "Tooltips to change tags on hover",
-                "F logo has purple hover color"
-            ],
-            removed: [
-                "Sample changelog entry"
-            ],
-            code: [
-                "Use variables for colors instead of hard-coding them",
-                "Changed 'modified' to 'updated'"
-            ]
-        }
-    },
-    {
-        type: "release",
-        project: "Website",
-        build: "19.7.8",
-        description: "Finally removed the placeholders and added a changelog",
-        changes: {
-            added: [
-                "This changelog"
-            ],
-            updated: [
-                "Navbar icons to stay on the right on all devices"
-            ],
-            fixed: [],
-            removed: [
-                "Placeholders",
-                "Mouse parallax effect"
-            ],
-            code: [
-                "Added basic HTML generation and dummy data in preparations for API calls"
-            ]
-        }
-    },
-    {
-        type: "release",
-        project: "Noter",
-        build: "19.7.8",
-        tags: ["alpha"],
-        changes: {
-            added: [
-                "Local storage to remember last theme used and to display that theme on next launch"
-            ]
-        }
-    },
-    {
-        type: "release",
-        project: "Tea-bot Re:Write",
-        build: "19.7.7",
-        tags: ["beta"],
-        changes: {
-            added: [
-                "Splash strings to config and their picker",
-                "Color subspecial",
-            ],
-            updated: [
-                "Discord onready event to display random splash string in the console",
-                "!info:about to use the random splash string"
-            ],
-        }
-    }
-];
+function renderTimeline(timelineData) {
 
-let timelineHtml = "";
+    let timelineHtml = "";
 
-for (let event of timelineData) {
-    let outputHtml = "";
-    if (event.type == "release") {
-        let buildColor = "";
+    for (let event of timelineData) {
+        let outputHtml = "";
+        if (event.type == "release") {
+            let buildColor = "";
+            let buildTag = "";
 
-        if (!event.description) { event.description = ""; }
+            if (!event.description) { event.description = ""; }
 
-        if (Array.isArray(event.tags)) {
-            if (event.tags.includes("alpha")) {
+            if (event.tag == "alpha") {
                 buildColor = "build-alpha";
+                buildTag = "alpha";
             }
-            if (event.tags.includes("beta")) {
+            if (event.tag == "beta") {
                 buildColor = "build-beta";
+                buildTag = "beta";
             }
+
+            outputHtml = /*html*/`
+                <div class="flow-changelog-release">
+                    <h3 class="release-title">${event.project} <span class="release-build ${buildColor}">${event.build} <span class="release-tag">${buildTag}</span></h3>
+                    <p class="release-notes">${event.description}</p>
+                    <div class="release-changes">${generateChangelogChangesHtml(event.changes)}</div>
+                </div>
+            `;
         }
-
-        outputHtml = /*html*/`
-            <div class="flow-changelog-release">
-                <h3 class="release-title">${event.project} <span class="release-build ${buildColor}">${event.build}</span> ${generateReleaseTagsHtml(event.tags)}</h3>
-                <p class="release-notes">${event.description}</p>
-                <div class="release-changes">${generateChangelogChangesHtml(event.changes)}</div>
-            </div>
-        `;
+        timelineHtml += outputHtml;
     }
-    timelineHtml += outputHtml;
-}
 
-$("#js-timeline").html(timelineHtml);
+    $("#js-timeline").html(timelineHtml);
+
+}
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
+    fetch('http://localhost:3211/api/timeline')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (timelineData) {
+            renderTimeline(timelineData)
+        });
 });
 
 const tailColors = [
@@ -209,7 +140,7 @@ function tailCycle() {
 
     let finalHtml = "";
 
-    let i=0;
+    let i = 0;
     for (let letter of tailText) {
         finalHtml += `<span style="color:${tailColors[i]}">${letter}</span>`
         i++;
