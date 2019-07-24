ef4b21f9f2ee1ec0e0bc55ebd40579ddb2bc8a1b
FajsiEx <fajsiexofficial@gmail.com>
2019-07-24 23:09:00 +0200

    + basic header style & > separated background from nav and got rid of the perma container

--- src/app/shell/header/header.component.ts
+++ src/app/shell/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
 
 @Component({
@@ -9,18 +9,31 @@ import { Router, NavigationStart } from '@angular/router';
 export class HeaderComponent implements OnInit {
 
   currentPage: string;
+  headerBackOpacity = 0;
 
-  constructor(private router: Router) {
-    this.router.events.subscribe(event=> {
+  constructor(private router: Router, private renderer: Renderer2) {
+    this.router.events.subscribe(event => {
       if (event instanceof NavigationStart) {
         console.log(event);
 
         this.currentPage = event.url.slice(1);
       }
     });
+
+    this.renderer.listen('window', 'scroll', () => { this.scrollHandler() });
   }
 
-  ngOnInit() {
+  scrollHandler() {
+    const scrollHeight = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (scrollHeight > 150) {
+      this.headerBackOpacity = 1;
+    } else {
+      this.headerBackOpacity = scrollHeight / 150;
+    }
+
+    console.log(this.headerBackOpacity);
   }
 
+  ngOnInit() {}
 }
