719d817abb85534e0a4e7ee46c1bd5bbde6b6ac8
FajsiEx <fajsiexofficial@gmail.com>
2019-07-24 21:29:46 +0200

    + issue event type

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -11,7 +11,7 @@ export class NewsComponent implements OnInit {
   events: any;
   fetchError = false;
 
-  filter:any = false; // Filter for the posts
+  filter: any = false; // Filter for the posts
 
   constructor(public wanilla: WanillaService) { }
 
@@ -31,10 +31,45 @@ export class NewsComponent implements OnInit {
   async fetchTimeline() {
     console.log(this.filter);
     try {
-      this.events = await this.wanilla.getTimeline(this.filter);
+      this.events = this.formatTimeline(await this.wanilla.getTimeline(this.filter));
     } catch (e) {
       this.fetchError = true;
       console.error(e);
     }
+  }
+
+  formatTimeline(timeline) { // Does some final formatting before timeline is displayed
+    console.log("ds")
+    timeline = this.formatTimestamps(timeline);
+    console.log("huhuh", timeline);
+    return timeline;
+  }
+
+  formatTimestamps(timeline) {
+    console.log("tstst", timeline)
+
+    for (const event of timeline) {
+      if (event.type != "issue") { continue; }
+
+      event.entries = event.entries.sort((x, y) => { return y.time - x.time });
+
+      for (const entry of event.entries) {
+        const entryDate = new Date(entry.time);
+
+        let [hours, minutes]: any[] = [
+          entryDate.getHours(),
+          entryDate.getMinutes()
+        ];
+
+        hours = (hours < 10) ? "0" + hours : hours;
+        minutes = (minutes < 10) ? "0" + minutes : minutes;
+
+        entry.time = entryDate.toLocaleDateString() + ` ${hours}:${minutes}`;
+      }
+
+      console.log("smth", event.entries, timeline);
+    }
+
+    return timeline;
   }
 }
