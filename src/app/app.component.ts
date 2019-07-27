bd859c0a29380245c9ccda1c08ebc90ec5d8ef7c
FajsiEx <fajsiexofficial@gmail.com>
2019-07-27 19:03:35 +0200

    > globilize the page-headers & + hover effect to filter reset link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Renderer2 } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component, Renderer2 } from '@angular/core';
 })
 export class AppComponent {
   title = 'fajsiex-web';
+  currentPage = '';
+  pageHeaderTransition = false;
+
+  pages = {
+    '': {
+      title: 'Home',
+      themeGradient: 'linear-gradient(to bottom right, #215aed, #4b36ed, #692be5)'
+    },
+    news: {
+      title: 'News & Changelog',
+      themeGradient: 'linear-gradient(to bottom right, #07f, #038)'
+    },
+    projects: {
+      title: 'Projects',
+      themeGradient: 'linear-gradient(to bottom right, #1a0, #060)'
+    },
+    about: {
+      title: 'About',
+      themeGradient: 'linear-gradient(to bottom right, #f80, #a60)'
+    },
+  };
+
+  constructor(private router: Router, private renderer: Renderer2) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        console.log(event);
+
+        this.pageHeaderTransition = true;
+        setTimeout(()=>{this.pageHeaderTransition = false;}, 500);
+
+        this.currentPage = event.url.slice(1);
+      }
+    });
+  }
 }
