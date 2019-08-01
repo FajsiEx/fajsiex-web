f2e9c54c3b7900ff95b2079e2674a16cb3cab5da
FajsiEx <fajsiexofficial@gmail.com>
2019-08-01 22:43:19 +0200

    * changed the way the header background is handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Renderer2 } from '@angular/core';
 import { Router, NavigationStart } from '@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -14,27 +15,33 @@ export class AppComponent {
   pages = {
     home: {
       title: 'Home',
-      themeGradient: 'linear-gradient(to bottom right, #215aed, #4b36ed, #692be5)'
+      themeGradient: 'linear-gradient(to bottom right, #215aed, #4b36ed, #692be5)',
+      themeColor: '#4b36ed'
     },
     '': {
       title: 'News & Changelog',
-      themeGradient: 'linear-gradient(to bottom right, #07f, #038)'
+      themeGradient: 'linear-gradient(to bottom right, #07f, #038)',
+      themeColor: '#06a'
     },
     news: {
       title: 'News & Changelog',
-      themeGradient: 'linear-gradient(to bottom right, #07f, #038)'
+      themeGradient: 'linear-gradient(to bottom right, #07f, #038)',
+      themeColor: '#06a'
     },
     projects: {
       title: 'Projects',
-      themeGradient: 'linear-gradient(to bottom right, #1a0, #060)'
+      themeGradient: 'linear-gradient(to bottom right, #1a0, #060)',
+      themeColor: '#0a6'
     },
     status: {
       title: 'Status',
-      themeGradient: 'linear-gradient(to bottom right, #a0a, #70a)'
+      themeGradient: 'linear-gradient(to bottom right, #a0a, #70a)',
+      themeColor: '#a0a'
     },
     about: {
       title: 'About',
-      themeGradient: 'linear-gradient(to bottom right, #f80, #a60)'
+      themeGradient: 'linear-gradient(to bottom right, #f80, #a60)',
+      themeColor: '#a60'
     },
   };
 
