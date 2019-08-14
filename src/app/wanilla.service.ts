ddc863e82fa115294e79fcd4aadcf1a7bf27df32
FajsiEx <fajsiexofficial@gmail.com>
2019-08-14 22:47:46 +0200

    * change to stabel tea-bot API

--- src/app/wanilla.service.ts
+++ src/app/wanilla.service.ts
@@ -8,8 +8,9 @@ export class WanillaService {
 
   //API_URL = 'http://localhost:3211/api/'; // localhost testing url
   API_URL = 'https://wanilla.eu-gb.mybluemix.net/api/';
-  TEABOT_API_URL = 'http://localhost:3210/api/';
-  //TEABOT_API_URL = 'https:///api/';
+
+  //TEABOT_API_URL = 'http://localhost:3210/api/';
+  TEABOT_API_URL = 'https://tea-bot.eu-gb.mybluemix.net/api/';
 
   async getTimeline(project, type) {
     if (!project) { project = 'all'; }
