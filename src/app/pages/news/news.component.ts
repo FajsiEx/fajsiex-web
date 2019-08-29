40a90f832dcb4dc71629fd2f1fce1d52a12199f9
FajsiEx <fajsiexofficial@gmail.com>
2019-08-29 15:35:57 +0200

    * stable release blue & * no stable build not bold

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -70,6 +70,7 @@ export class NewsComponent implements OnInit {
           alpha: (project.alpha) ? project.alpha.split(' ')[0] : false,
           beta: (project.beta) ? project.beta.split(' ')[0] : false,
           stable: (project.stable) ? project.stable : 'No stable version',
+          hasStable: !!project.stable
         }
       });
     }
